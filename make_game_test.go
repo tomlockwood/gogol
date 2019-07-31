77b7f8a8912f9a7f4edb44f15a908acaeacbda81
Tom Lockwood <tom.lockwood@isentia.com>
2019-07-31 20:47:52 +1000

    Make Game test

---
+++ make_game_test.go
@@ -0,0 +1,26 @@
+package gol
+
+import "testing"
+
+// MakeGame testing
+
+// Checking that permutations of the rules work
+
+var opts = Options{
+	0,
+	0,
+	Grid{},
+	0,
+	Rules{}}
+
+func TestDefaultLoad(t *testing.T) {
+	game := MakeGame(opts)
+
+	if game.X != 50 {
+		t.Fatalf("X Coordinate not set to default of 50")
+	}
+
+	if game.Y != 50 {
+		t.Fatalf("Y Coordinate not set to default of 50")
+	}
+}
