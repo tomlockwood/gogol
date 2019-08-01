17df783bbe968ea4790ab492a4ad55f56f08cf8a
Tom Lockwood <tom.lockwood@isentia.com>
2019-08-01 18:43:22 +1000

    Benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -10,18 +10,25 @@ func tick(g Game, gameNumber int) {
 	}
 }
 
-func BenchmarkRunManyConways(b *testing.B) {
+var cr0 = Rule{false, [9]uint8{0, 0, 0, 1, 0, 0, 0, 0, 0}, Colour{}}
+var cr1 = Rule{true, [9]uint8{0, 0, 1, 1, 0, 0, 0, 0, 0}, Colour{}}
+var crs = Rules{[]Rule{r0, r1}}
 
-	var r0 = Rule{false, [9]uint8{0, 0, 0, 1, 0, 0, 0, 0, 0}, Colour{}}
-	var r1 = Rule{true, [9]uint8{0, 0, 1, 1, 0, 0, 0, 0, 0}, Colour{}}
-	var rs = Rules{[]Rule{r0, r1}}
+var conwayOpts = Options{
+	0,
+	0,
+	Grid{},
+	2,
+	crs}
 
-	opts := Options{
-		0,
-		0,
-		Grid{},
-		2,
-		rs}
+func BenchRunMany(opts Options, gameAmount int, TickFunction TickFunction, b *testing.B) {
+	RunMany(opts, gameAmount, TickFunction)
+}
 
-	RunMany(opts, 1000, tick)
+func Benchmark1000Conways(b *testing.B) {
+	RunMany(conwayOpts, 1000, tick)
+}
+
+func Benchmark100Conways(b *testing.B) {
+	RunMany(conwayOpts, 100, tick)
 }
